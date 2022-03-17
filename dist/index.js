186e19c315e7162c7e6c9e282b4ef230d5b5bce2
Mert Sert <mertseert@gmail.com>
2022-03-18 01:56:30 +0300

    Updated dist file for typescripts.

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,4 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./server"));
-server_1.default.init(9090);
+server_1.default.listen(9090);
--- dist/services/httpclient.js
+++ dist/services/httpclient.js
@@ -5,14 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
 class HttpClient {
-    constructor(url) {
+    constructor(baseURL) {
         this._initializeResponseInterceptor = () => {
             this.instance.interceptors.response.use(this._handleResponse, this._handleError);
         };
         this._handleResponse = ({ data }) => data;
         this._handleError = (error) => Promise.reject(error);
         this.instance = axios_1.default.create({
-            url,
+            baseURL,
         });
         this._initializeResponseInterceptor();
     }
